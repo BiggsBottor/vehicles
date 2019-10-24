656e1fc23c0574b0cebf1ff7de6aed5752b87fdd
BiggsBottor <jonatan.lledo@gmail.com>
2019-10-24 12:55:43 +0200

    typesript FASE 1 validando el formulario para crear el vehiculo

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,8 +1,32 @@
 "use strict";
+/*-- valores del coche --*/
+var _plate = document.querySelector("#plate"); //matrícula
+var _brand = document.querySelector("#brand"); //marca
+var _color = document.querySelector("#color"); //color
+/*-- llama a la función al pulsar el botón --*/
+var submit = document.querySelector("#createCar");
+submit.addEventListener("click", function () {
+    createCar(_plate.value, _brand.value, _color.value);
+});
+/*-- crea y añade el HTML para mostrar los datos recogidos --*/
 function createCar(plate, brand, color) {
     var car = new Car(plate, color, brand);
-    car.addWheel(new Wheel(2, "SEAT"));
-    document.body.innerText = "CAR: PLATE: " + car.plate
-        + " COLOR: " + car.color + " BRAND: " + brand
-        + " WHEELS: " + JSON.stringify(car.wheels);
+    var section = document.querySelector("#carInfo");
+    section.innerHTML = "";
+    var p1 = document.createElement("p");
+    var p2 = document.createElement("p");
+    var node1 = document.createTextNode("Car:");
+    var node2 = "<b>PLATE</b>: " + plate + " <br><b>BRAND</b>: " + brand + " <br><b>COLOR</b>: " + color;
+    p1.appendChild(node1);
+    p2.innerHTML = node2;
+    section.appendChild(p1);
+    section.appendChild(p2);
 }
+/*-- back up --*/
+/* function createCar(plate:string,brand:string,color:string){
+    let car=new Car(plate,color,brand);
+    car.addWheel(new Wheel(2,"SEAT"));
+    document.body.innerText="CAR: PLATE: " + car.plate
+    + " COLOR: " +car.color + " BRAND: " + brand
+    + " WHEELS: " + JSON.stringify(car.wheels);
+} */
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -1,9 +1,35 @@
+/*-- valores del coche --*/
+var _plate = <HTMLInputElement>document.querySelector("#plate"); //matrícula
+var _brand = <HTMLInputElement>document.querySelector("#brand"); //marca
+var _color = <HTMLInputElement>document.querySelector("#color"); //color
 
+/*-- llama a la función al pulsar el botón --*/
+var submit = <HTMLElement>document.querySelector("#createCar");
+submit.addEventListener("click", function() {
+    createCar(_plate.value, _brand.value, _color.value)
+});
+
+/*-- crea y añade el HTML para mostrar los datos recogidos --*/
 function createCar(plate:string,brand:string,color:string){
+    let car=new Car(plate,color,brand);
+    let section = <HTMLElement>document.querySelector("#carInfo");
+    section.innerHTML = "";
+    let p1 = document.createElement("p");
+    let p2 = document.createElement("p");
+    let node1 = document.createTextNode("Car:");
+    let node2 = `<b>PLATE</b>: ${plate} <br><b>BRAND</b>: ${brand} <br><b>COLOR</b>: ${color}`;
+    p1.appendChild(node1);
+    p2.innerHTML = node2;
+    section.appendChild(p1);
+    section.appendChild(p2);
+}
+/*-- back up --*/
+
+/* function createCar(plate:string,brand:string,color:string){
     let car=new Car(plate,color,brand);
     car.addWheel(new Wheel(2,"SEAT"));
     document.body.innerText="CAR: PLATE: " + car.plate 
     + " COLOR: " +car.color + " BRAND: " + brand 
     + " WHEELS: " + JSON.stringify(car.wheels);
-}
+} */
 
