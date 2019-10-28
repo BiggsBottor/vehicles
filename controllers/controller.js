2c6ad54d5ecb53479cd426cdb4535a0ef295002f
BiggsBottor <jonatan.lledo@gmail.com>
2019-10-28 12:54:33 +0100

    typesript FASE 2 Añadiendo ruedas al coche

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,30 +3,72 @@
 var _plate = document.querySelector("#plate"); //matrícula
 var _brand = document.querySelector("#brand"); //marca
 var _color = document.querySelector("#color"); //color
+var sectionWheels = document.querySelector("#createWheelsForm");
+var wheelsTxt = ["Front Left", "Front Right", "Back Left", "Back Right"];
+var submitCar = document.querySelector("#createCarBtn");
+var submitWheels = document.querySelector("#createWheelsBtn");
 /*-- llama a la función al pulsar el botón --*/
-var submit = document.querySelector("#createCar");
-submit.addEventListener("click", function () {
+submitCar.addEventListener("click", function () {
     createCar(_plate.value, _brand.value, _color.value);
+    submitCar.disabled = true;
+    submitCar.classList.toggle("btn-primary");
+    submitCar.classList.toggle("btn-secondary");
+    sectionWheels.classList.remove("d-none");
 });
+var currentCar;
 /*-- crea y añade el HTML para mostrar los datos recogidos --*/
 function createCar(plate, brand, color) {
     var car = new Car(plate, color, brand);
+    currentCar = car;
     var section = document.querySelector("#carInfo");
     section.innerHTML = "";
-    var p1 = document.createElement("p");
-    var p2 = document.createElement("p");
+    //crea el titulo
+    var h4 = document.createElement("h4");
+    h4.classList.add("mb-4");
     var node1 = document.createTextNode("Car:");
-    var node2 = "<b>PLATE</b>: " + plate + " <br><b>BRAND</b>: " + brand + " <br><b>COLOR</b>: " + color;
-    p1.appendChild(node1);
-    p2.innerHTML = node2;
-    section.appendChild(p1);
-    section.appendChild(p2);
+    h4.appendChild(node1);
+    section.appendChild(h4);
+    //crea el texto del coche
+    var p = document.createElement("p");
+    var node2 = "<b>PLATE</b>: " + plate + ", <b>BRAND</b>: " + brand + ", <b>COLOR</b>: " + color;
+    p.innerHTML = node2;
+    section.appendChild(p);
 }
-/*-- back up --*/
-/* function createCar(plate:string,brand:string,color:string){
-    let car=new Car(plate,color,brand);
-    car.addWheel(new Wheel(2,"SEAT"));
-    document.body.innerText="CAR: PLATE: " + car.plate
-    + " COLOR: " +car.color + " BRAND: " + brand
-    + " WHEELS: " + JSON.stringify(car.wheels);
-} */
+var wheelBrand = document.querySelectorAll(".wheel_brand");
+var wheelDiameter = document.querySelectorAll(".wheel_diameter");
+var wheelsBrand = Array.prototype.slice.call(wheelBrand);
+var wheelsDiameter = Array.prototype.slice.call(wheelDiameter);
+submitWheels.addEventListener("click", function () {
+    CreateWheels(currentCar);
+    submitWheels.disabled = true;
+    submitWheels.classList.toggle("btn-primary");
+    submitWheels.classList.toggle("btn-secondary");
+});
+/*-- crea ruedas de 1 en 1 para pasarle al coche creado --*/
+function createWheel(diameter, brand) {
+    var w = new Wheel(diameter, brand);
+    currentCar.addWheel(w);
+}
+/*-- obtiene los datos de las ruedas para añadirlas al coche actual y mostralo por pantalla --*/
+function CreateWheels(c) {
+    for (var w_1 = 0; w_1 < 4; w_1++) {
+        // añadiendo el símbolo "+" delante de la variable, se convierte en tipo number
+        createWheel(+wheelsDiameter[w_1].value, wheelsBrand[w_1].value);
+    }
+    var section = document.querySelector("#wheelsInfo");
+    section.innerHTML = "";
+    //crea el titulo
+    var h4 = document.createElement("h4");
+    h4.classList.add("mb-4");
+    var node1 = document.createTextNode("Wheels:");
+    h4.appendChild(node1);
+    section.appendChild(h4);
+    //crea el texto de las ruedas
+    var p = document.createElement("p");
+    var w = c.wheels;
+    for (var i = 0; i < w.length; i++) {
+        var node = wheelsTxt[i] + "=> <b>Brand:</b> " + w[i].brand + " <b>Diameter:</b> " + w[i].diameter + "<br>";
+        p.innerHTML += node;
+    }
+    section.appendChild(p);
+}
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -3,33 +3,83 @@ var _plate = <HTMLInputElement>document.querySelector("#plate"); //matrícula
 var _brand = <HTMLInputElement>document.querySelector("#brand"); //marca
 var _color = <HTMLInputElement>document.querySelector("#color"); //color
 
+var sectionWheels = <HTMLElement>document.querySelector("#createWheelsForm");
+const wheelsTxt = ["Front Left", "Front Right", "Back Left", "Back Right"];
+
+var submitCar = <HTMLButtonElement>document.querySelector("#createCarBtn");
+var submitWheels = <HTMLButtonElement>document.querySelector("#createWheelsBtn");
+
 /*-- llama a la función al pulsar el botón --*/
-var submit = <HTMLElement>document.querySelector("#createCar");
-submit.addEventListener("click", function() {
+submitCar.addEventListener("click", function() {
     createCar(_plate.value, _brand.value, _color.value)
+    submitCar.disabled = true;
+    submitCar.classList.toggle("btn-primary");
+    submitCar.classList.toggle("btn-secondary");
+    sectionWheels.classList.remove("d-none");
 });
+
+var currentCar:Car;
 
 /*-- crea y añade el HTML para mostrar los datos recogidos --*/
 function createCar(plate:string,brand:string,color:string){
-    let car=new Car(plate,color,brand);
+    let car=new Car(plate,color,brand);    
+    currentCar = car;    
     let section = <HTMLElement>document.querySelector("#carInfo");
     section.innerHTML = "";
-    let p1 = document.createElement("p");
-    let p2 = document.createElement("p");
+    //crea el titulo
+    let h4 = document.createElement("h4");
+    h4.classList.add("mb-4");
     let node1 = document.createTextNode("Car:");
-    let node2 = `<b>PLATE</b>: ${plate} <br><b>BRAND</b>: ${brand} <br><b>COLOR</b>: ${color}`;
-    p1.appendChild(node1);
-    p2.innerHTML = node2;
-    section.appendChild(p1);
-    section.appendChild(p2);
+    h4.appendChild(node1);
+    section.appendChild(h4);
+    //crea el texto del coche
+    let p = document.createElement("p");
+    let node2 = `<b>PLATE</b>: ${plate}, <b>BRAND</b>: ${brand}, <b>COLOR</b>: ${color}`;
+    p.innerHTML = node2;
+    section.appendChild(p);
+
 }
-/*-- back up --*/
 
-/* function createCar(plate:string,brand:string,color:string){
-    let car=new Car(plate,color,brand);
-    car.addWheel(new Wheel(2,"SEAT"));
-    document.body.innerText="CAR: PLATE: " + car.plate 
-    + " COLOR: " +car.color + " BRAND: " + brand 
-    + " WHEELS: " + JSON.stringify(car.wheels);
-} */
+var wheelBrand:NodeListOf<HTMLInputElement> = document.querySelectorAll(".wheel_brand");
+var wheelDiameter:NodeListOf<HTMLInputElement> = document.querySelectorAll(".wheel_diameter");
+var wheelsBrand: HTMLInputElement[] = Array.prototype.slice.call(wheelBrand);
+var wheelsDiameter: HTMLInputElement[] = Array.prototype.slice.call(wheelDiameter);
 
+
+submitWheels.addEventListener("click", function(){
+    CreateWheels(currentCar);
+    submitWheels.disabled = true;
+    submitWheels.classList.toggle("btn-primary");
+    submitWheels.classList.toggle("btn-secondary");
+});
+
+
+/*-- crea ruedas de 1 en 1 para pasarle al coche creado --*/
+function createWheel(diameter:number, brand:string){
+     var w = new Wheel(diameter, brand);     
+     currentCar.addWheel(w);
+}
+
+/*-- obtiene los datos de las ruedas para añadirlas al coche actual y mostralo por pantalla --*/
+function CreateWheels(c:Car){    
+    for (let w = 0; w < 4; w++){
+        // añadiendo el símbolo "+" delante de la variable, se convierte en tipo number
+        createWheel(+wheelsDiameter[w].value, wheelsBrand[w].value); 
+    }    
+    let section = <HTMLElement>document.querySelector("#wheelsInfo");
+    section.innerHTML = "";
+    //crea el titulo
+    let h4 = document.createElement("h4");
+    h4.classList.add("mb-4");
+    let node1 = document.createTextNode("Wheels:");
+    h4.appendChild(node1);
+    section.appendChild(h4);
+    //crea el texto de las ruedas
+    let p = document.createElement("p");
+    let w = c.wheels;        
+    for (let i = 0; i < w.length; i++){
+        let node = `${wheelsTxt[i]}=> <b>Brand:</b> ${w[i].brand} <b>Diameter:</b> ${w[i].diameter}<br>`;
+        p.innerHTML += node;
+    }
+    section.appendChild(p);    
+}
