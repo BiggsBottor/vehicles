32e0878260c9e4003427d619bdcdd9608bf47bdb
BiggsBottor <jonatan.lledo@gmail.com>
2019-10-29 13:40:37 +0100

    typesript FASE 3 Añadiendo filtros de comprobación de datos

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,19 +3,31 @@
 var _plate = document.querySelector("#plate"); //matrícula
 var _brand = document.querySelector("#brand"); //marca
 var _color = document.querySelector("#color"); //color
-var sectionWheels = document.querySelector("#createWheelsForm");
-var wheelsTxt = ["Front Left", "Front Right", "Back Left", "Back Right"];
+/*-- patrón para comprobar la matrícula --*/
+var platePattern = /\d{4}[A-Z]{3}/;
+var rePlate = new RegExp(platePattern);
+/*-- botones de creación --*/
 var submitCar = document.querySelector("#createCarBtn");
 var submitWheels = document.querySelector("#createWheelsBtn");
 /*-- llama a la función al pulsar el botón --*/
 submitCar.addEventListener("click", function () {
-    createCar(_plate.value, _brand.value, _color.value);
-    submitCar.disabled = true;
-    submitCar.classList.toggle("btn-primary");
-    submitCar.classList.toggle("btn-secondary");
-    sectionWheels.classList.remove("d-none");
+    if (_plate.value != "" && _brand.value != "" && _color.value != "") {
+        if (rePlate.test(_plate.value)) {
+            createCar(_plate.value, _brand.value, _color.value);
+            submitCar.disabled = true;
+            submitCar.classList.toggle("btn-primary");
+            submitCar.classList.toggle("btn-secondary");
+            sectionWheels.classList.remove("d-none");
+        }
+        else {
+            alert("El formato de matr\u00EDcula correcto es 0000AAA");
+        }
+    }
+    else {
+        alert("Por favor, rellena los espacios en blanco");
+    }
 });
-var currentCar;
+var currentCar; // variable de control para mantener los datos creados del coche
 /*-- crea y añade el HTML para mostrar los datos recogidos --*/
 function createCar(plate, brand, color) {
     var car = new Car(plate, color, brand);
@@ -34,15 +46,33 @@ function createCar(plate, brand, color) {
     p.innerHTML = node2;
     section.appendChild(p);
 }
-var wheelBrand = document.querySelectorAll(".wheel_brand");
-var wheelDiameter = document.querySelectorAll(".wheel_diameter");
-var wheelsBrand = Array.prototype.slice.call(wheelBrand);
-var wheelsDiameter = Array.prototype.slice.call(wheelDiameter);
+/*-- sección de las ruedas --*/
+var sectionWheels = document.querySelector("#createWheelsForm");
+var wheelsTxt = ["Front Left", "Front Right", "Back Left", "Back Right"];
+var wheelBrand = document.querySelectorAll(".wheel_brand"); //marcas de las ruedas
+var wheelDiameter = document.querySelectorAll(".wheel_diameter"); //diámetro de las ruedas
+var wheelsBrand = Array.prototype.slice.call(wheelBrand); //array de las marcas
+var wheelsDiameter = Array.prototype.slice.call(wheelDiameter); // array de los diámteros
+var brk = false; // variable que controla si habrá errores
 submitWheels.addEventListener("click", function () {
-    CreateWheels(currentCar);
-    submitWheels.disabled = true;
-    submitWheels.classList.toggle("btn-primary");
-    submitWheels.classList.toggle("btn-secondary");
+    var empty = 0;
+    for (var w = 0; w < 4; w++) {
+        if (wheelsDiameter[w].value != "" && wheelsBrand[w].value != "") {
+            empty++;
+        }
+        else {
+            alert("Por favor, rellena los espacios en blanco");
+            break;
+        }
+    }
+    if (empty == 4) {
+        CreateWheels(currentCar);
+        if (!brk) { // si no han habido errores, se inhabilita el botón
+            submitWheels.disabled = true;
+            submitWheels.classList.toggle("btn-primary");
+            submitWheels.classList.toggle("btn-secondary");
+        }
+    }
 });
 /*-- crea ruedas de 1 en 1 para pasarle al coche creado --*/
 function createWheel(diameter, brand) {
@@ -51,24 +81,49 @@ function createWheel(diameter, brand) {
 }
 /*-- obtiene los datos de las ruedas para añadirlas al coche actual y mostralo por pantalla --*/
 function CreateWheels(c) {
-    for (var w_1 = 0; w_1 < 4; w_1++) {
+    brk = false;
+    for (var w = 0; w < 4; w++) {
         // añadiendo el símbolo "+" delante de la variable, se convierte en tipo number
-        createWheel(+wheelsDiameter[w_1].value, wheelsBrand[w_1].value);
+        if (isNaN(+wheelsDiameter[w].value)) { // "NO es un numero"            
+            brk = true;
+            alert("El diametro de la rueda DEBE ser un número");
+            break;
+        }
+        else { // "Es un numero"            
+            if (+wheelsDiameter[w].value > 0.4 && +wheelsDiameter[w].value < 2) {
+                createWheel(+wheelsDiameter[w].value, wheelsBrand[w].value);
+            }
+            else {
+                brk = true;
+                alert("el diámetro de la rueda debe ser entre 0.4 y 2 (no incluidos)");
+                break;
+            }
+        }
     }
-    var section = document.querySelector("#wheelsInfo");
-    section.innerHTML = "";
-    //crea el titulo
-    var h4 = document.createElement("h4");
-    h4.classList.add("mb-4");
-    var node1 = document.createTextNode("Wheels:");
-    h4.appendChild(node1);
-    section.appendChild(h4);
-    //crea el texto de las ruedas
-    var p = document.createElement("p");
-    var w = c.wheels;
-    for (var i = 0; i < w.length; i++) {
-        var node = wheelsTxt[i] + "=> <b>Brand:</b> " + w[i].brand + " <b>Diameter:</b> " + w[i].diameter + "<br>";
-        p.innerHTML += node;
+    if (!brk) //si no se ha interrumpido el rellenado de las ruedas del coche, se muestran por pantalla
+     {
+        var section = document.querySelector("#wheelsInfo");
+        section.innerHTML = "";
+        //crea el titulo
+        var h4 = document.createElement("h4");
+        h4.classList.add("mb-4");
+        var node1 = document.createTextNode("Wheels:");
+        h4.appendChild(node1);
+        section.appendChild(h4);
+        //crea el texto de las ruedas
+        var p = document.createElement("p");
+        var w = c.wheels;
+        debugger;
+        for (var i = 0; i < w.length; i++) {
+            var node = wheelsTxt[i] + "=> <b>Brand:</b> " + w[i].brand + " <b>Diameter:</b> " + w[i].diameter + "<br>";
+            p.innerHTML += node;
+        }
+        section.appendChild(p);
     }
-    section.appendChild(p);
+    else //si se ha interrumpido el rellenado del array de ruedas, se vacia
+     {
+        if (c.wheels.length < 4) {
+            c.wheels.length = 0;
+        }
+    }
 }
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -2,23 +2,32 @@
 var _plate = <HTMLInputElement>document.querySelector("#plate"); //matrícula
 var _brand = <HTMLInputElement>document.querySelector("#brand"); //marca
 var _color = <HTMLInputElement>document.querySelector("#color"); //color
+/*-- patrón para comprobar la matrícula --*/
+var platePattern = /\d{4}[A-Z]{3}/;
+var rePlate = new RegExp(platePattern);
 
-var sectionWheels = <HTMLElement>document.querySelector("#createWheelsForm");
-const wheelsTxt = ["Front Left", "Front Right", "Back Left", "Back Right"];
-
+/*-- botones de creación --*/
 var submitCar = <HTMLButtonElement>document.querySelector("#createCarBtn");
 var submitWheels = <HTMLButtonElement>document.querySelector("#createWheelsBtn");
 
 /*-- llama a la función al pulsar el botón --*/
 submitCar.addEventListener("click", function() {
-    createCar(_plate.value, _brand.value, _color.value)
-    submitCar.disabled = true;
-    submitCar.classList.toggle("btn-primary");
-    submitCar.classList.toggle("btn-secondary");
-    sectionWheels.classList.remove("d-none");
+    if (_plate.value != "" && _brand.value != "" && _color.value != ""){
+        if (rePlate.test(_plate.value)){
+            createCar(_plate.value, _brand.value, _color.value);
+            submitCar.disabled = true;
+            submitCar.classList.toggle("btn-primary");
+            submitCar.classList.toggle("btn-secondary");
+            sectionWheels.classList.remove("d-none");
+        } else {
+            alert(`El formato de matrícula correcto es 0000AAA`);
+        }    
+    } else {
+        alert("Por favor, rellena los espacios en blanco");
+    }
 });
 
-var currentCar:Car;
+var currentCar:Car; // variable de control para mantener los datos creados del coche
 
 /*-- crea y añade el HTML para mostrar los datos recogidos --*/
 function createCar(plate:string,brand:string,color:string){
@@ -40,19 +49,41 @@ function createCar(plate:string,brand:string,color:string){
 
 }
 
-var wheelBrand:NodeListOf<HTMLInputElement> = document.querySelectorAll(".wheel_brand");
-var wheelDiameter:NodeListOf<HTMLInputElement> = document.querySelectorAll(".wheel_diameter");
-var wheelsBrand: HTMLInputElement[] = Array.prototype.slice.call(wheelBrand);
-var wheelsDiameter: HTMLInputElement[] = Array.prototype.slice.call(wheelDiameter);
+/*-- sección de las ruedas --*/
+var sectionWheels = <HTMLElement>document.querySelector("#createWheelsForm");
+const wheelsTxt = ["Front Left", "Front Right", "Back Left", "Back Right"];
 
+var wheelBrand:NodeListOf<HTMLInputElement> = document.querySelectorAll(".wheel_brand"); //marcas de las ruedas
+var wheelDiameter:NodeListOf<HTMLInputElement> = document.querySelectorAll(".wheel_diameter"); //diámetro de las ruedas
+var wheelsBrand: HTMLInputElement[] = Array.prototype.slice.call(wheelBrand); //array de las marcas
+var wheelsDiameter: HTMLInputElement[] = Array.prototype.slice.call(wheelDiameter); // array de los diámteros
 
+var brk = false; // variable que controla si habrá errores
 submitWheels.addEventListener("click", function(){
-    CreateWheels(currentCar);
-    submitWheels.disabled = true;
-    submitWheels.classList.toggle("btn-primary");
-    submitWheels.classList.toggle("btn-secondary");
+    var empty = 0;
+    for (let w = 0; w < 4; w++)
+    {
+        if (wheelsDiameter[w].value != "" && wheelsBrand[w].value != "")
+        {
+            empty++;            
+        }
+        else 
+        {
+            alert("Por favor, rellena los espacios en blanco");
+            break;
+        }         
+    }
+    if (empty == 4)    
+    {        
+        CreateWheels(currentCar);
+        if (!brk){ // si no han habido errores, se inhabilita el botón
+            submitWheels.disabled = true;
+            submitWheels.classList.toggle("btn-primary");
+            submitWheels.classList.toggle("btn-secondary");
+        }
+    }
+    
 });
-
 
 /*-- crea ruedas de 1 en 1 para pasarle al coche creado --*/
 function createWheel(diameter:number, brand:string){
@@ -61,25 +92,56 @@ function createWheel(diameter:number, brand:string){
 }
 
 /*-- obtiene los datos de las ruedas para añadirlas al coche actual y mostralo por pantalla --*/
-function CreateWheels(c:Car){    
-    for (let w = 0; w < 4; w++){
+function CreateWheels(c:Car){
+    brk = false;
+    for (let w = 0; w < 4; w++)
+    {
         // añadiendo el símbolo "+" delante de la variable, se convierte en tipo number
-        createWheel(+wheelsDiameter[w].value, wheelsBrand[w].value); 
-    }    
-    let section = <HTMLElement>document.querySelector("#wheelsInfo");
-    section.innerHTML = "";
-    //crea el titulo
-    let h4 = document.createElement("h4");
-    h4.classList.add("mb-4");
-    let node1 = document.createTextNode("Wheels:");
-    h4.appendChild(node1);
-    section.appendChild(h4);
-    //crea el texto de las ruedas
-    let p = document.createElement("p");
-    let w = c.wheels;        
-    for (let i = 0; i < w.length; i++){
-        let node = `${wheelsTxt[i]}=> <b>Brand:</b> ${w[i].brand} <b>Diameter:</b> ${w[i].diameter}<br>`;
-        p.innerHTML += node;
+        if (isNaN(+wheelsDiameter[w].value))
+        {// "NO es un numero"            
+            brk = true;
+            alert("El diametro de la rueda DEBE ser un número");
+            break;
+        } 
+        else 
+        {// "Es un numero"            
+            if (+wheelsDiameter[w].value > 0.4 && +wheelsDiameter[w].value < 2)
+            {                
+                createWheel(+wheelsDiameter[w].value, wheelsBrand[w].value);
+            }
+            else 
+            {
+                brk = true;
+                alert("el diámetro de la rueda debe ser entre 0.4 y 2 (no incluidos)");
+                break;
+            }
+        }
     }
-    section.appendChild(p);    
+    if (!brk) //si no se ha interrumpido el rellenado de las ruedas del coche, se muestran por pantalla
+    {        
+        let section = <HTMLElement>document.querySelector("#wheelsInfo");
+        section.innerHTML = "";
+        //crea el titulo
+        let h4 = document.createElement("h4");
+        h4.classList.add("mb-4");
+        let node1 = document.createTextNode("Wheels:");
+        h4.appendChild(node1);
+        section.appendChild(h4);
+        //crea el texto de las ruedas
+        let p = document.createElement("p");
+        let w = c.wheels;
+        debugger;
+        for (let i = 0; i < w.length; i++){
+            let node = `${wheelsTxt[i]}=> <b>Brand:</b> ${w[i].brand} <b>Diameter:</b> ${w[i].diameter}<br>`;
+            p.innerHTML += node;
+        }
+        section.appendChild(p);    
+    } 
+    else //si se ha interrumpido el rellenado del array de ruedas, se vacia
+    {       
+       if (c.wheels.length < 4)
+       {
+           c.wheels.length = 0;
+       }       
+    }
 }
